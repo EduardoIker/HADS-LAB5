5520787d61f0a0b19209e5e98027c2a1237f2446
EduardoIker <eduardoikersw@gmail.com>
2017-03-21 17:58:38 +0100

    Add files via upload

---
+++ Inicio.aspx.vb
@@ -0,0 +1,33 @@
+﻿Imports AccesoDatos.accesoDatosSQL
+
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        conectar()
+    End Sub
+
+    Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
+        cerrarConexion()
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim resultado As Integer
+        resultado = login(TextBox1.Text, TextBox2.Text)
+        If (resultado = 0) Then
+            Session.Contents("correo") = TextBox1.Text
+            Session.Contents("tipo") = "A"
+            Response.Redirect("Alumno.aspx")
+        ElseIf (resultado = 1) Then
+            Session.Contents("correo") = TextBox1.Text
+            Session.Contents("tipo") = "P"
+            Response.Redirect("Profesor.aspx")
+        ElseIf (resultado = -2) Then
+            Label2.Text = "Datos de acceso incorrectos. Inténtalo de nuevo."
+        Else
+            Label2.Text = "Ha ocurrido un problema inesperado. Inténtalo de nuevo."
+        End If
+    End Sub
+
+
+End Class
---
+++ Registro.aspx.vb
@@ -0,0 +1,50 @@
+﻿Imports AccesoDatos.accesoDatosSQL
+
+Public Class WebForm2
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        conectar()
+    End Sub
+
+    Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
+        cerrarConexion()
+    End Sub
+
+    Protected Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nombre As String
+        Dim apellidos As String
+        Dim dni As String
+        Dim password As String
+        Dim pregunta As String
+        Dim respuesta As String
+        Dim correo As String
+        Dim res As String
+        Dim rol = "A"
+
+        nombre = TextBox9.Text
+        apellidos = TextBox3.Text
+        password = TextBox5.Text
+        pregunta = TextBox7.Text
+        correo = TextBox2.Text
+        dni = TextBox4.Text
+        respuesta = TextBox8.Text
+        If (RadioButton1.Checked) Then
+            rol = "P"
+        End If
+        res = insertar(correo, nombre, apellidos, pregunta, respuesta, dni, password, rol)
+        Label2.Text = res
+    End Sub
+
+    Protected Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Protected Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+End Class
